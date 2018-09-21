45a8fb7291398e92ed2e22de5ae8ce4629158be7
Farzad Moghaddampoor <f.moghaddampoor@gmail.com>
2018-09-21 21:49:25 +0200

    Completed movie class

--- Vidly/Vidly/Models/Movie.cs
+++ Vidly/Vidly/Models/Movie.cs
@@ -5,7 +5,12 @@ using System.Web;
 
 namespace Vidly.Models
 {
+    /// <summary>
+    /// Movies controller and action random
+    /// </summary>
     public class Movie
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
